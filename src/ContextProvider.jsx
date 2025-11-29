17e7ced1d1c0c86a408d9bfc0f73768f8e16a645
afnan328 <muhammedafnan4791@gmail.com>
2025-11-29 10:39:12 +0530

    updateds

--- src/ContextProvider.jsx
+++ src/ContextProvider.jsx
@@ -3,15 +3,15 @@ import { createContext } from 'react'
 
 export const UserContext=createContext();
 
-export default function ContextProvider({Children}) {
+export default function ContextProvider({children}) {
 
-    // const host ="http://localhost:7000"
-    const host="https://server-zs9y.onrender.com"
+    const host ="http://localhost:7000"
+    // const host="https://server-zs9y.onrender.com"
 
   return (
     <div>
       <UserContext.Provider value={{host}}>
-      {Children}
+      {children}
       </UserContext.Provider>
     </div>
   )
